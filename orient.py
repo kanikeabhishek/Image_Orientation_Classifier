ba7afa458167c1e8cd134c38868e588ae9f22485
“Chuhua <whale9067@hotmail.com>
2017-12-05 22:08:48 -0500

    helper function

--- orient.py
+++ orient.py
@@ -1,13 +1,83 @@
 import random
+import numpy as np
+
+
 ##### Neural Network
 
 
+# read text file
+def read_data(filename):
+    """
+    :param filename: file name
+    :return: list of lists including 1 class column(0,90,180,270) and 192d features, converted into integer
+    """
+    with open(filename, 'r') as file:
+
+        #for row in file:
+            #each_data = map(int, row.split()[2:])
+            #print len(each_data)
+        return [map(int, row.split()[1:]) for row in file]
+        #print type(dataset),len(dataset)
+
+# extract X and y
+def extract_feature_class(dataset):
+    """
+    :param dataset: extract X and y from dataset read from file
+    :return: list of X and list of Y
+    """
+    X=[]
+    y=[]
+    for each_data in dataset:
+        X.append(each_data[1:])
+        y.append(each_data[0])
+    return X,y
+
+
 # initialize a network
-def initialize_NN():
-    return []
+def initialize_NN(n_hiddenlayer):
+    """
+    :param n_hiddenlayer: number of hidden layer
+    :return: list of lists, each list is a layer
+    """
+    return [[] for x in range(n_hiddenlayer+1)] # hidden layer + 1 output layer
 
-def initialize_weight():
-    return
 
+def initialize_layer_weight(input_neuron, neuron):
+    return [{'w': [random.random() for x in range(input_neuron + 1)]} for x in range(neuron)]
+
+
+def construct_network(nnetwork, input_neuron, output_neuron, *hidden_neuron):
+    """
+    :param network: initial network with no weight
+    :param input_neuron: number of neuron in input layer
+    :param output_neuron: number of neuron in output layer
+    :param hidden_neuron: number of neuron in hidden layer (length should equals to number of layers)
+    :return: neural network with weights and bias
+    """
+
+    # if no hidden layer, only output layer
+    if len(nnetwork) ==1:
+        nnetwork[0] = initialize_layer_weight(input_neuron, output_neuron)
+    # 1 hidden layer
+    elif len(nnetwork) ==2:
+        nnetwork[0] = initialize_layer_weight(input_neuron, hidden_neuron[0])
+        nnetwork[1] = initialize_layer_weight(hidden_neuron[0], output_neuron)
+    # multilayer
+    else:
+        for i in range(len(nnetwork)):
+            if i ==0:
+                nnetwork[i]=initialize_layer_weight(input_neuron,hidden_neuron[i])
+            elif i == len(nnetwork)-1:
+                nnetwork[i] = initialize_layer_weight(hidden_neuron[i], output_neuron)
+            else:
+                nnetwork[i] = initialize_layer_weight(hidden_neuron[i-1], hidden_neuron[i])
+    print nnetwork
 def activation():
     return
+
+
+train = read_data('test-data.txt')
+extract_feature_class(train)
+
+nn = initialize_NN(1)
+construct_network(nn,2,2,1)
